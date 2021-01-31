e2d5eaa824beff3949399dea62a89a1bd9cd75ee
sowonday <kksw5324@gmail.com>
2021-01-31 22:54:43 +0900

    Initial Commit

---
+++ SwMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwMemo
+//
+//  Created by 김소원 on 2021/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
